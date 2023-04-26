ae502b293ea19c6464eb853fdd195889e9c76592
Mohamed Hassan <57665567+mamh4@users.noreply.github.com>
2023-04-26 21:58:00 +0200

    Added psudo code implementation of the moving functionality + testing git changes from VS after including gitignore file.

--- predators/predators/Animal.h
+++ predators/predators/Animal.h
@@ -1,5 +1,39 @@
 #pragma once
 class Animal
 {
+/* psudo code implementatin of the move function
+animal.step_size <- 4
+
+move <- function(){
+  #call utility score function: A function that determines and returns a coordinate of the best viable option
+  haven <- score_function()
+  dx <- animal.posX-haven.posX
+  dy <- animal.posY-haven.posY
+  
+  in_radius <- sqrt(dx^2+dy^2) <= haven.radius
+  
+  if(!in_radius){
+    
+    # Determine the direction of movement for each axis
+    if (dx > 0) {
+      x_dir <- min(dx,animal.step_size)
+    } else if (dx < 0) {
+      x_dir <- -min(dx,animal.step_size)
+    }
+    
+    if (dy > 0) {
+      y_dir <- min(dy,animal.step_size)
+    } else if (dy < 0) {
+      y_dir <- -min(dy,animal.step_size)
+    }
+    
+   animal.set_position(animapl.posX+x_dir,animal.posY+y_dir) 
+    
+  }
+  
+
+}
+	*/
+
 };
 
